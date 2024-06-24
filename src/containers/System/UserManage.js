4fce408530c3dff0c4c3399db919018b519466b5
Lee Ngoc <lee.ngoc6462@gmail.com>
2024-06-24 22:58:00 +0700

    Design Modal create new user with Reactstrap

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,12 +3,14 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss'
 import { getAllUsers } from '../../services/userService';
+import ModalUser from './ModalUser';
 class UserManage extends Component {
     
     constructor(props) {
         super(props);
         this.state = {
             arrUser: [],
+            isOpenModalUser: false,
         }
     }
     
@@ -16,8 +18,8 @@ class UserManage extends Component {
      *  Life cycle
      * Run component
      * 1. Run construct -> init state
-     * 2. Did mount
-     * 3. Render
+     * 2. Did mount (set state) : born ; unmount
+     * 3. Render (re-render)
      */
 
     async componentDidMount() {
@@ -29,13 +31,36 @@ class UserManage extends Component {
         }
     }
 
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true,
+        })
+    }
+
+    toggleUserModal = () => {
+		this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+	}
 
     render() {
         // console.log("Check render", this.state)
         let arrUsers = this.state.arrUsers;
         return (
             <div className="users-container">
+                <ModalUser
+                    isOpen ={this.state.isOpenModalUser}
+                    toggleFromParent={this.toggleUserModal}
+                    test={'abc'}
+                    
+                />
                 <div className='title text-center'>Manage user with leengoc</div>
+                <div className='mx-1'>
+                    <button 
+                    className='btn btn-primary px-3'
+                    onClick={() =>this.handleAddNewUser()}
+                    ><i class="fas fa-plus"></i> Add new user</button>
+                </div>
                 <div className='users-table mt-3 mx-1'>
                     <table id="customers">
                         <tr>
@@ -45,9 +70,8 @@ class UserManage extends Component {
                             <th>Address</th>
                             <th>Action</th>
                         </tr>
-                       
                             { arrUsers && arrUsers.map((item, index) => {
-                                console.log('Check map', item, index)
+                                {/* console.log('Check map', item, index) */}
                                 return (
                                     <tr>
                                         <td>{item.email}</td>
@@ -61,16 +85,12 @@ class UserManage extends Component {
                                     </tr>
                                 )
                             })
-
-                            }
-                            
-                        
+                            }   
                     </table>
                 </div>
             </div>
         );
     }
-
 }
 
 const mapStateToProps = state => {
